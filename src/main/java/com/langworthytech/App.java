097188b6f9348cdeb2ecda8cbdb2887e1dcb5dfa
Kent Hervey <kent.hervey@gmail.com>
2020-03-27 15:03:57 -0500

    removed comment as requested

--- src/main/java/com/langworthytech/App.java
+++ src/main/java/com/langworthytech/App.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class App {
 
-	//this should only appear in kentdev
+	
     public static void main(String[] args) {
 
         Vehicle mazda = new Vehicle("2008", "Mazda", "6", "i Sport");
